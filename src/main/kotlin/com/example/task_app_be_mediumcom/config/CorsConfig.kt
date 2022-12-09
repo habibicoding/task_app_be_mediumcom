742535bf69808255d2e973095bc7eba9e6aefb0d
habibicoding <habibicoding@yahoo.com>
2022-12-09 20:58:48 +0100

    Add cors config class.

---
+++ src/main/kotlin/com/example/task_app_be_mediumcom/config/CorsConfig.kt
@@ -0,0 +1,22 @@
+package com.example.task_app_be_mediumcom.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun getCorsConfiguration(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+            }
+        }
+    }
+}
