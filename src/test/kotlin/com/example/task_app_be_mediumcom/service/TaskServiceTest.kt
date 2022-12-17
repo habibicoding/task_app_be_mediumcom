9ad61a6c5f5df69e829e8d631b7e823fb1ac5014
habibicoding <habibicoding@yahoo.com>
2022-12-17 01:10:46 +0100

    Adapt class.

--- src/test/kotlin/com/example/task_app_be_mediumcom/service/TaskServiceTest.kt
+++ src/test/kotlin/com/example/task_app_be_mediumcom/service/TaskServiceTest.kt
@@ -1,5 +1,16 @@
 package com.example.task_app_be_mediumcom.service
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
 
-internal class TaskServiceTest
+import org.junit.jupiter.api.Assertions.*
+import io.mockk.junit5.MockKExtension
+
+
+@ExtendWith(MockKExtension::class)
+internal class TaskServiceTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+}
