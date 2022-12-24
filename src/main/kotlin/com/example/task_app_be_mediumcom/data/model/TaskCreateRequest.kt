e0978608a5995080399a5518e140e3912750893e
habibicoding <habibicoding@yahoo.com>
2022-12-24 23:50:42 +0100

    Remove unnecessary field.

--- src/main/kotlin/com/example/task_app_be_mediumcom/data/model/TaskCreateRequest.kt
+++ src/main/kotlin/com/example/task_app_be_mediumcom/data/model/TaskCreateRequest.kt
@@ -4,18 +4,15 @@ import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
 
 data class TaskCreateRequest(
-        @NotBlank(message = "task id can't be empty")
-        val id: Long,
+    @NotBlank(message = "description can't be empty")
+    val description: String,
 
-        @NotBlank(message = "description can't be empty")
-        val description: String,
+    val isReminderSet: Boolean,
 
-        val isReminderSet: Boolean,
+    val isTaskOpen: Boolean,
 
-        val isTaskOpen: Boolean,
+    @NotBlank(message = "created_on can't be empty")
+    val createdOn: LocalDateTime,
 
-        @NotBlank(message = "created_on can't be empty")
-        val createdOn: LocalDateTime,
-
-        val priority: Priority
+    val priority: Priority
 )
