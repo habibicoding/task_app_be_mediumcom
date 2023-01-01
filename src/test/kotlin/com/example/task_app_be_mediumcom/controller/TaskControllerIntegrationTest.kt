9608c54fd7755a2d9bf5cb4418351c5783794aa3
habibicoding <habibicoding@yahoo.com>
2023-01-01 18:23:37 +0100

    Rename method.

--- src/test/kotlin/com/example/task_app_be_mediumcom/controller/TaskControllerIntegrationTest.kt
+++ src/test/kotlin/com/example/task_app_be_mediumcom/controller/TaskControllerIntegrationTest.kt
@@ -73,7 +73,7 @@ internal class TaskControllerIntegrationTest(@Autowired private val mockMvc: Moc
     }
 
     @Test
-    fun `given one task when get task by id is called with string instead of int then check for bad request`() {
+    fun `when get task by id is called with string instead of integer then check for bad request`() {
         val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/task/404L"))
 
         resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest)
